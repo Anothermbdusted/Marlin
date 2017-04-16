685a2a0a2f1d82296a02fa478bbab2d606bb6381
Thomas Moore <tcm0116@gmail.com>
2017-04-15 22:20:16 -0500

    Set default state of volumetric extrusion to on

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1106,7 +1106,7 @@
  *
  * M200 D0 to disable, M200 Dn to set a new diameter.
  */
-//#define VOLUMETRIC_DEFAULT_ON
+#define VOLUMETRIC_DEFAULT_ON
 
 /**
  * Enable this option for a leaner build of Marlin that removes all
